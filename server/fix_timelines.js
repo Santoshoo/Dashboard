f7aa8fdc219d7640f5f08d5eda86cefb4c7200c2
Amlan <2710amlan@gmail.com>
2026-09-02 16:36:16 +0530

    update

--- server/fix_timelines.js
+++ server/fix_timelines.js
@@ -5,7 +5,7 @@ const Movement = require('./models/Movement');
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD || process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
